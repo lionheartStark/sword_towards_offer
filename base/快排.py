e50250901d43592c638c5b2f180123145bafbb62
qy <1003146780@qq.com>
2020-06-27 16:01:13 +0800

    做了好一些题目

--- base/快排.py
+++ base/快排.py
@@ -1,33 +1,33 @@
-def part(a_list, start, end):
+def part(the_list, start, end):
     i = start
     j = end
 
     while (i < j):
-        while i < j and a_list[i] <= a_list[j]:
+        while i < j and the_list[i] <= the_list[j]:
             j -= 1
         if i < j:
-            tmp = a_list[i]
-            a_list[i] = a_list[j]
-            a_list[j] = tmp
+            tmp = the_list[i]
+            the_list[i] = the_list[j]
+            the_list[j] = tmp
             i += 1
-        while i < j and a_list[i] <= a_list[j]:
+        while i < j and the_list[i] <= the_list[j]:
             i += 1
         if i < j:
-            tmp = a_list[i]
-            a_list[i] = a_list[j]
-            a_list[j] = tmp
+            tmp = the_list[i]
+            the_list[i] = the_list[j]
+            the_list[j] = tmp
             j -= 1
     return i
 
 
-def mt(a_list, start, end):
+def mt(the_list, start, end):
     if start < end:
-        p = part(a_list, start, end)
-        mt(a_list, start, p - 1)
-        mt(a_list, p + 1, end)
+        p = part(the_list, start, end)
+        mt(the_list, start, p - 1)
+        mt(the_list, p + 1, end)
 
 
-a_list = [5, 4, 6, 3, 28, 13]
+the_list = [5, 4, 6, 3, 28, 13]
 
-mt(a_list, 0, len(a_list) - 1)
-print(a_list)
+mt(the_list, 0, len(the_list) - 1)
+print(the_list)
