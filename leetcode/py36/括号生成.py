43e0157d937deb7cd72ab7faeea92fe46b077084
qy <1003146780@qq.com>
2020-07-04 10:13:56 +0800

    做了好一些题目

--- leetcode/py36/括号生成.py
+++ leetcode/py36/括号生成.py
@@ -31,23 +31,22 @@ class Solution:
         print(self.res)
         return self.res
 
-    class Solution:
-        def generateParenthesis(self, n: int) -> List[str]:
-            res = []
-            cur_str = ''
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        cur_str = ''
 
-            def dfs(cur_str, left, right):
-                if left == 0 and right == 0:
-                    res.append(cur_str)
-                    return
-                if right < left:
-                    return
-                if left > 0:
-                    dfs(cur_str + '(', left - 1, right)
-                if right > 0:
-                    dfs(cur_str + ')', left, right - 1)
+        def dfs(cur_str, left, right):
+            if left == 0 and right == 0:
+                res.append(cur_str)
+                return
+            if right < left:
+                return
+            if left > 0:
+                dfs(cur_str + '(', left - 1, right)
+            if right > 0:
+                dfs(cur_str + ')', left, right - 1)
 
-            dfs(cur_str, n, n)
-            return res
+        dfs(cur_str, n, n)
+        return res
 
 Solution().generateParenthesis(3)
