6cfc1e803717a899e1248966dc8ddb05f443151f
qy <1003146780@qq.com>
2020-07-06 16:00:11 +0800

    做了好一些题目

--- base/二分插入排序.py
+++ base/二分插入排序.py
@@ -1,23 +1,38 @@
-def twofen_idx(a, left, right, num):
-    while left < right:
-        mid = (left + right) // 2
-        if num > a[mid]:
-            left = mid + 1
+
+def bisect_right(a, x, lo=0, hi=None):
+    """Return the index where to insert item x in list a, assuming a is sorted.
+
+    The return value i is such that all e in a[:i] have e < x, and all e in
+    a[i:] have e >= x.  So if x already appears in the list, a.insert(x) will
+    insert just before the leftmost x already there.
+
+    Optional args lo (default 0) and hi (default len(a)) bound the
+    slice of a to be searched.
+    """
+
+    if lo < 0:
+        raise ValueError('lo must be non-negative')
+    if hi is None:
+        hi = len(a)
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if a[mid] > x:
+            hi = mid
         else:
-            right = mid - 1
-    return left
+            lo = mid + 1
+    return lo
 
 
 def twofen_insert_sort(a, n):
     for i in range(1, n):
         if a[i - 1] > a[i]:
-            j = twofen_idx(a, 0, i - 1, a[i])
+            j = bisect_right(a, a[i], 0, i)
             shaobing = a[i]
             for k in range(i - 1, j - 1, -1):
                 a[k + 1] = a[k]
             a[j] = shaobing
 
 
-a = [1, 3, 2, 4, 5]
+a = [1, 2, 3, 2, 1]
 twofen_insert_sort(a, 5)
 print(a)
